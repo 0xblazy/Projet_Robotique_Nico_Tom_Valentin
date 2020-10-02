30f1d96762e95d18a23dee7be24bbecd6aee4c5e
nicolas-carbonnier <nicoinferno55@gmail.com>
2020-10-02 09:43:10 +0200

    Create BumperCar

---
+++ src/Twister/BumperCar.java
@@ -0,0 +1,13 @@
+package Twister;
+
+import lejos.hardware.Button;
+
+public class BumperCar {
+
+	public static void main(String[] args) {
+		System.out.println("Appuyez pour démarrer");
+		Button.waitForAnyPress();
+
+	}
+
+}
