216a8374e444655850471803c0b2af6df3725983
val <val@val-UX302LA>
2020-11-13 09:17:13 +0100

    mise en place bluetooth, fichier de config, création base régle de jeu

---
+++ src/twister/models/communication/Bluetooth.java
@@ -0,0 +1,5 @@
+package twister.models.communication;
+
+public class Bluetooth {
+
+}
---
+++ src/twister/models/communication/Recepteur.java
@@ -0,0 +1,57 @@
+package twister.models.communication;
+
+import java.io.DataInputStream;
+import java.io.InputStream;
+import lejos.hardware.Button;
+import lejos.hardware.lcd.LCD;
+import lejos.remote.nxt.BTConnector;
+import lejos.remote.nxt.NXTConnection;
+/**
+ * Classe recepteur du projet
+ * @author val
+ *
+ */
+
+
+public class Recepteur {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String connected = "Connecté";
+		String waiting = "En attente";
+		
+		try {
+			//LCD.drawString(waiting, 0, 0);
+			//LCD.refresh();
+
+			BTConnector bt = new BTConnector();
+			NXTConnection btc = bt.waitForConnection(100000, NXTConnection.PACKET);
+
+			if (btc !=null) {
+			LCD.clear();
+			LCD.drawString(connected, 0, 0);
+			LCD.refresh();
+
+			InputStream is = btc.openInputStream();
+			//OutputStream os = btc.openOutputStream();
+			DataInputStream dis = new DataInputStream(is);
+			//DataOutputStream dos = new DataOutputStream(os);
+
+			int valeur = dis.read();
+			
+
+			dis.close();
+			//dos.close();
+			btc.close();
+			System.out.println(valeur);
+			Button.RIGHT.waitForPressAndRelease();
+			LCD.clear();
+			} else {
+				System.out.println("Pas de connexion");
+				Button.RIGHT.waitForPressAndRelease();
+			}
+		} catch (Exception e) {
+		}
+	}
+
+}
