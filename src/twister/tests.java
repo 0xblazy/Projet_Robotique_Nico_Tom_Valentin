00517642b61e339d9aa939e16741ea4613fefe27
Tomy <scheffmanntom@gmail.com>
2020-11-20 09:27:32 +0100

    val bg

--- src/twister/tests.java
+++ src/twister/tests.java
@@ -4,7 +4,14 @@ public class tests {
 
 public static void main(String[] args) {
 	
-	int[][] liste = new int[17][2];
-	System.out.println(liste.length);
+	int[][] cartoTest= {
+			{4,4,4,5,2},
+			{4,5,5,5,4},
+			{4,3,3,5,4},
+			{4,5,1,5,4},
+			{4,5,4,3,4},
+			{1,5,5,3,4},
+			{4,4,4,4,4}
+	};
 }
 }
