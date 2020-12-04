86ce90bade5b5773e616ddb124bd0d4c1483c34b
nicolas-carbonnier <nicoinferno55@gmail.com>
2020-12-04 16:15:03 +0100

    Correction getRndom

--- src/twister/models/ReglesJeu.java
+++ src/twister/models/ReglesJeu.java
@@ -7,7 +7,7 @@ public interface ReglesJeu extends TwisterColor {
 	
 	
 	public static int getRandom() {
-	    int rnd = new Random().nextInt(COLORS_CODE.length);
+	    int rnd = 1 + new Random().nextInt(COLORS_CODE.length - 1);
 	    System.out.println(COLORS[rnd]);
 	    return COLORS_CODE[rnd];
 	}
